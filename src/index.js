62df50291cf6e132a8866878d73fb7b1182d0cf3
jagadeesh5225 <77929740+jagadeesh5225@users.noreply.github.com>
2023-07-31 11:51:11 +0000

    add

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <BrowserRouter basename={"/"}>
+     <BrowserRouter basename={process.env.PUBLIC_URL}>
     {window.innerWidth < 500 ? <AppMobile /> : <App />} 
     </BrowserRouter>
 
