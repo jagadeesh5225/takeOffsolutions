f75c365d6d6d0a289488c39b41d54a8fba6a3c5f
jagadeesh5225 <77929740+jagadeesh5225@users.noreply.github.com>
2023-07-31 11:42:15 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import AppMobile from "./AppMobile";
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,   Routes as Switch,Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <BrowserRouter basename={process.env.PUBLIC_URL}>
-    {window.innerWidth < 500 ? <AppMobile /> : <App />}
-    </BrowserRouter>
+     {/* <BrowserRouter basename={"/"}>
+     <Switch>
+        <Route path='/' component=/>
+        </Switch>
+    </BrowserRouter> */}
+
+    {window.innerWidth < 500 ? <AppMobile /> : <App />} 
   </React.StrictMode>
 );
