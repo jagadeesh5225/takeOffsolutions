09eb949b7e2ac3b701d89655415186fc4bc87067
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2024-04-26 08:39:35 +0530

    done

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,8 +2,8 @@ import React from "react";
 import AppMobile from "./AppMobile";
 import App from "./App";
 
-const MainApp = () => {
+function MainApp() {
   return <div>{window.innerWidth < 500 ? <AppMobile /> : <App />} </div>;
-};
+}
 
 export default MainApp;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import MainApp from "./MainApp";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  // BrowserRouter,
+  // Route,
+  // Routes,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" Component={<MainApp />} />
-    </Routes>
-   </BrowserRouter>
+    <RouterProvider
+      router={createBrowserRouter([{ path: "/", element: <MainApp /> }])}
+      // fallbackElement={<LoadingOutlined color="#023595" />}
+    />
+    {/* <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" Component={<MainApp />} />
+      </Routes>
+    </BrowserRouter> */}
   </React.StrictMode>
 );
